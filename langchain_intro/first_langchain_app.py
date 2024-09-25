27a7ad4abe9ca1ffef7173d4373534091dd06a1c
Souparna Bose <souparnabose99@gmail.com>
2024-09-25 12:43:50 +0530

    Update project files

--- langchain_intro/first_langchain_app.py
+++ langchain_intro/first_langchain_app.py
@@ -3,3 +3,17 @@ from langchain_openai import ChatOpenAI
 
 if __name__ == "__main__":
     print("First LangChain App")
+
+    summary_template = """
+    given the information {information} about a person from I want to create:
+    1. a short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(input_variables="information", template=summary_template)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # temperature decides how creative the language model is
+
+    chain = summary_prompt_template | llm
+    
+
