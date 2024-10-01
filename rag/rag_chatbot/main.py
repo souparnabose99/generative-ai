a9681ab9c1582f25668b6a8286cc75cc7450ee8f
Souparna Bose <souparnabose99@gmail.com>
2024-10-01 11:46:51 +0530

    Update project files

--- rag/rag_chatbot/main.py
+++ rag/rag_chatbot/main.py
@@ -24,3 +24,54 @@ documents = loader.load()
 template = "you are senior developer who answers {question}  based on your knowledge based on {context}"
 prompt = PromptTemplate(template=template, input_variables=["question", "context"])
 
+# RETRIEVER - Load embeddings and create a vector store
+embeddings = OpenAIEmbeddings()
+vector_store = FAISS.from_documents(documents, embeddings)
+
+
+# GENERATE - Define the function to generate the response
+def generate(query):
+    chain_type_kwargs = {"prompt": prompt}
+    chain = RetrievalQA.from_chain_type(llm=model, chain_type="stuff", retriever=vector_store.as_retriever(search_kwargs={"k": 1}), chain_type_kwargs=chain_type_kwargs,)
+    response = chain.run(query)
+    return response
+
+
+def start():
+    print("OPENAI API KEY", os.getenv("OPENAI_API_KEY"))
+    print("documents", documents)
+    instructions = (
+        "Type your question and press ENTER. Type 'x' to go back to the MAIN menu.\n"
+    )
+    print(Fore.RED + "\n\x1B[3m" + instructions + "\x1B[0m" + Fore.RESET)
+
+    print("MENU")
+    print("====")
+    print("[1]- Ask a question")
+    print("[2]- Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        ask()
+    elif choice == "2":
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid choice")
+        start()
+
+
+def ask():
+    while True:
+        user_input = input("Q: ")
+        # Exit
+        if user_input == "x":
+            start()
+        else:
+            response = generate(user_input)
+
+            print(Fore.RED + f"A: " + response + Fore.RESET)
+            print(Fore.WHITE + "\n-------------------------------------------------")
+
+
+if __name__ == "__main__":
+    start()
