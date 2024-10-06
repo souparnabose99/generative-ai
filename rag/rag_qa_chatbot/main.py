dd6024bd82cf1ae9121781c31eeed2c3f3ca0ea1
Souparna Bose <souparnabose99@gmail.com>
2024-10-06 12:43:55 +0530

    Update project files

--- rag/rag_qa_chatbot/main.py
+++ rag/rag_qa_chatbot/main.py
@@ -73,11 +73,38 @@ def query(query_str):
 
 
 def start():
-    pass
+    instructions = (
+        "Type your question and press ENTER. Type 'x' to go back to the MAIN menu.\n"
+    )
+    print(Fore.BLUE + "\n\x1B[3m" + instructions + "\x1B[0m" + Fore.RESET)
+
+    print("MENU")
+    print("====")
+    print("[1]- Ask a question")
+    print("[2]- Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        ask()
+    elif choice == "2":
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid choice")
+        start()
 
 
 def ask():
-    pass
+    while True:
+        user_input = input("Q: ")
+        # Exit
+        if user_input == "x":
+            start()
+        else:
+
+            response = query(user_input)
+
+            print(Fore.BLUE + f"A: " + response + Fore.RESET)
+            print(Fore.WHITE + "\n-------------------------------------------------")
 
 
 if __name__ == "__main__":
