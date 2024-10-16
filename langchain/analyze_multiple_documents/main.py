fe4f863aff257c876f7eae1ae55b88b1433b1f17
Souparna Bose <souparnabose99@gmail.com>
2024-10-17 00:30:08 +0530

    Update project files

---
+++ langchain/analyze_multiple_documents/main.py
@@ -0,0 +1,36 @@
+import os
+from pydantic import BaseModel, Field
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import Tool
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains import RetrievalQA
+os.environ['OPENAI_API_KEY'] = "sk-xxx"
+
+
+class DocumentInput(BaseModel):
+    question: str = Field()
+
+
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+
+tools = []
+files = [
+    {
+        "name": "alphabet-earnings",
+        "path": "https://abc.xyz/investor/static/pdf/2023Q1\
+        _alphabet_earnings_release.pdf",
+    },
+    {
+        "name": "Cisco-earnings",
+        "path": "https://d18rn0p25nwr6d.cloudfront.net/CIK-00\
+            00858877/5b3c172d-f7a3-4ecb-b141-03ff7af7e068.pdf",
+    },
+    {
+        "name": "IBM-earnings",
+        "path": "https://www.ibm.com/investor/att/pdf/IBM_\
+            Annual_Report_2022.pdf",
+    },
+    ]
