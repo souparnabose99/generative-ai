6d49569818555bcc0b80acb435753ddb98345f3c
Souparna Bose <souparnabose99@gmail.com>
2024-10-27 04:15:23 +0530

    Update project files

---
+++ langchain/seek_and_summarize_profile/linkedin.py
@@ -0,0 +1,13 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool=False):
+    """Scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+    pass
+
+
