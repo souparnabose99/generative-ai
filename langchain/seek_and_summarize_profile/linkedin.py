f9a8b5501910ee1c56f6ef1a2aa9002ce07422e4
Souparna Bose <souparnabose99@gmail.com>
2024-10-29 12:30:46 +0530

    Update project files

--- langchain/seek_and_summarize_profile/linkedin.py
+++ langchain/seek_and_summarize_profile/linkedin.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool=False):
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     """Scrape information from LinkedIn profiles,
     Manually scrape the information from the LinkedIn profile"""
 
@@ -19,10 +19,15 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool=False):
 
     data = response.json()
 
+    data = {k: v for k, v in data.items()
+            if v not in ([], "", None) and k not in ["people_also_viewed", "certifications"]}
+
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
     return data
 
 
 if __name__ == "__main__":
     print(scrape_linkedin_profile(linkedin_profile_url="", mock=True))
-
-
