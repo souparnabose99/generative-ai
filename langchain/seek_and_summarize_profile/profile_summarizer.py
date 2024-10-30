063dfb9542d0aa4925d598180e4718c6e62dd445
Souparna Bose <souparnabose99@gmail.com>
2024-10-30 12:33:54 +0530

    Update project files

---
+++ langchain/seek_and_summarize_profile/profile_summarizer.py
@@ -0,0 +1,8 @@
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+
