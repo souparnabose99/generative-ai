cbb449550a37eaddd193b270f3210280aea5b2a9
Souparna Bose <souparnabose99@gmail.com>
2024-11-13 12:36:09 +0530

    Update project files

--- langchain/seek_and_summarize_profile/profile_summarizer.py
+++ langchain/seek_and_summarize_profile/profile_summarizer.py
@@ -6,3 +6,11 @@ from dotenv import load_dotenv
 if __name__ == "__main__":
     load_dotenv()
 
+    print("Profile Summarizer")
+
+    summary_template = """
+    given the information {information} about a person, I want you to create:
+    1. A short summary
+    2. 2 interesting facts about them
+    """
+
