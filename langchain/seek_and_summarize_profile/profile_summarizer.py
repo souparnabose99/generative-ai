696d65cdea652a0bf94a8d76518dc8c89850aed9
Souparna Bose <souparnabose99@gmail.com>
2024-11-14 12:39:18 +0530

    Update project files

--- langchain/seek_and_summarize_profile/profile_summarizer.py
+++ langchain/seek_and_summarize_profile/profile_summarizer.py
@@ -14,3 +14,9 @@ if __name__ == "__main__":
     2. 2 interesting facts about them
     """
 
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={"information": information})
+    print(res)
