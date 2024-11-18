04ed78be9d16e8e97ab995cddd05bc200f6dbb2b
Souparna Bose <souparnabose99@gmail.com>
2024-11-18 13:28:17 +0530

    Update project files

--- langraph/langraph_job_resume_search/tools.py
+++ langraph/langraph_job_resume_search/tools.py
@@ -17,3 +17,20 @@ def process_resume() -> Resume:
     return resume
 
 
+@tool
+def get_job(field: Optional[Literal['title', 'company', 'location', 'salary', 'description', 'responsibilities', 'benefits', 'employment_type', 'posted_date']] = None) -> str:
+    """Get job data."""
+    job = process_job()
+    if field:
+        return getattr(job, field)
+    return job.dict()
+
+
+@tool
+def get_resume(field: Optional[Literal['name', 'professional_summary', 'work_experience', 'education', 'skills']] = None) -> str:
+    """Get resume data."""
+    resume = process_resume()
+    if field:
+        return getattr(resume, field)
+    return resume.dict()
+
