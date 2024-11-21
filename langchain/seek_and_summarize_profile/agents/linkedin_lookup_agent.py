8250087db23408be600159886fe6d6c771f460d2
Souparna Bose <souparnabose99@gmail.com>
2024-11-21 14:46:50 +0530

    Update project files

--- langchain/seek_and_summarize_profile/agents/linkedin_lookup_agent.py
+++ langchain/seek_and_summarize_profile/agents/linkedin_lookup_agent.py
@@ -13,5 +13,22 @@ load_dotenv()
 
 
 def lookup(name: str) -> str:
+    llm = ChatOpenAI(
+        temperature=0,
+        model_name="gpt-4o-mini"
+    )
+
+    template = """
+    given the full name of a person {name_of_a_person} I want you to get me a link to their LinkedIn profile page.
+    Your answer should contain only a url
+    """
+
+    prompt_template = PromptTemplate(
+        template=template,
+        input_variables=["name_of_a_person"]
+    )
+
+    
+
     return "https://www.linked.com/in/eden-marco/"
 
