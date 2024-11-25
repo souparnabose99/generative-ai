5da5e19e97cdf73bc64ea37f570d64a6ac7a1433
Souparna Bose <souparnabose99@gmail.com>
2024-11-25 12:42:25 +0530

    Update project files

--- langchain/seek_and_summarize_profile/profile_summarizer.py
+++ langchain/seek_and_summarize_profile/profile_summarizer.py
@@ -4,6 +4,7 @@ from langchain.chains import LLMChain
 from dotenv import load_dotenv
 from linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.output_parser import summary_parser
 
 
 def summarize_profile(name: str) -> str:
@@ -14,9 +15,14 @@ def summarize_profile(name: str) -> str:
         given the LinkedIn information {information} about a person, I want you to create:
         1. A short summary
         2. 2 interesting facts about them
+        
+        \n{format_instructions}
         """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()})
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = summary_prompt_template | llm
