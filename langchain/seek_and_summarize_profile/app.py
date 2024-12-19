f46de268ca19c34b45776fee2e4123ca20b05934
Souparna Bose <souparnabose99@gmail.com>
2024-12-20 00:08:41 +0530

    Update project files

--- langchain/seek_and_summarize_profile/app.py
+++ langchain/seek_and_summarize_profile/app.py
@@ -12,20 +12,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/process", methods=["POST"])
-def process():
-    name = request.form["name"]
-    summary_and_facts, interests, profile_information, profile_pic_url = summarize_profile(name=name)
-    return jsonify(
-        {
-            "summary_and_facts": summary_and_facts.to_dict(),
-            "interests": interests.to_dict(),
-            "profile_information": profile_information.to_dict(),
-            "picture_url": profile_pic_url,
-        }
-    )
 
 
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", debug=True)
+
