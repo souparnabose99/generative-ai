119686ba052be7f439b66efeb11e6e001f901b1d
Souparna Bose <souparnabose99@gmail.com>
2024-12-21 01:32:43 +0530

    Update project files

--- langchain/react_langchain/main.py
+++ langchain/react_langchain/main.py
@@ -11,6 +11,6 @@ def get_text_length(text:str) -> int:
 
 
 if __name__ == "__main__":
-    print(get_text_length(text="abcde"))
+    # print(get_text_length(text="abcde"))
     # get_text_length is no longer a function but a Tool
-    print(get_text_length.invoke())
+    print(get_text_length.invoke(input={"text": "abdegj"}))
