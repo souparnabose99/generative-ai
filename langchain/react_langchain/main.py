92de62cee8375c2aa0004b49a2f1377df0393b5d
Souparna Bose <souparnabose99@gmail.com>
2024-12-22 01:42:53 +0530

    Update project files

--- langchain/react_langchain/main.py
+++ langchain/react_langchain/main.py
@@ -15,4 +15,26 @@ if __name__ == "__main__":
     # print(get_text_length(text="abcde"))
     # get_text_length is no longer a function but a Tool
     tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought:{agent_scratchpad}
+    """
     print(get_text_length.invoke(input={"text": "abdegj"}))
