03350981da1573605de9695b6d22946238167118
Souparna Bose <souparnabose99@gmail.com>
2024-12-28 11:08:30 +0530

    Update project files

--- langchain/personal_doc_chatbot/personal_doc_explainer.py
+++ langchain/personal_doc_chatbot/personal_doc_explainer.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Upload pdf files
 st.header("Personal Doc Analyzer Chatbot")
@@ -9,6 +11,20 @@ with st.sidebar:
     file = st.file_uploader("Upload a pdf & ask your questions", type="pdf")
 
 # Extract text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+        # st.write(text)
 
 # Perform data chunking
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n"],
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
 
