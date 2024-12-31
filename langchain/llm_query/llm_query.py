4f79f498feeef9c741c9de5aee47a8c795fe3ccd
Souparna Bose <souparnabose99@gmail.com>
2024-12-31 17:35:16 +0530

    Update project files

---
+++ langchain/llm_query/llm_query.py
@@ -0,0 +1,9 @@
+from langchain.llms import OpenAI
+OPENAI_API_KEY = "xxxxxx"
+
+
+if __name__ == "__main__":
+    llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+    res = llm("Share useful information about our solar system")
+    print(res)
+
