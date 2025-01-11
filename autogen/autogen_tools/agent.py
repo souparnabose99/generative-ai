35ec4186f7f11d799efcdbadf0339120d30676c6
Souparna Bose <souparnabose99@gmail.com>
2025-01-11 20:11:08 +0530

    Update project files

--- autogen/autogen_tools/agent.py
+++ autogen/autogen_tools/agent.py
@@ -1,5 +1,13 @@
 import os
-from autogen import AssistantAgent, UserProxyAgent
+from autogen import AssistantAgent, UserProxyAgent, ConversableAgent
 from dotenv import load_dotenv
 
 load_dotenv()
+
+model = "gpt-4o-mini"
+
+llm_config = {
+    "model": model,
+    "api_key": os.environ.get("OPENAI_API_KEY")
+}
+
