c823344ab9b2b7f4ca98f41eebdbf91aa4d0882c
Souparna Bose <souparnabose99@gmail.com>
2025-01-12 20:39:15 +0530

    Update project files

---
+++ autogen/autogen_agents/assisstant_proxy_agent.py
@@ -0,0 +1,29 @@
+import os
+from autogen import AssistantAgent, UserProxyAgent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = "gpt-4o-mini"
+llm_config = {
+    "model": model,
+    "api_key": os.environ.get("OPENAI_API_KEY"),
+}
+
+assistant = AssistantAgent("assistant", llm_config)
+user_proxy = UserProxyAgent(
+    "user_proxy",
+    llm_config=llm_config,
+    code_execution_config={
+        "workd_dir": "code_execution",
+        "use_docker": False,
+    },
+    human_input_mode="NEVER",
+)
+
+# start the agents
+user_proxy.initiate_chat(
+    assistant,
+    message="What is the capital of Russia?",
+)
+
