87311fcd9f5eca185027d831dff938e2fa93a419
Souparna Bose <souparnabose99@gmail.com>
2025-01-25 12:48:31 +0530

    Update project files

--- langchain/vector_dbs/vector_db_ingestion.py
+++ langchain/vector_dbs/vector_db_ingestion.py
@@ -10,13 +10,15 @@ load_dotenv()
 
 if __name__ == '__main__':
     print("Data Ingestion started...")
-    loader = TextLoader(r"blogfilepath.txt")
+    loader = TextLoader(r"E:\Coding-Mastery\Lang-Chain\vector_dbs\medium-blogs.txt")
     document = loader.load()
 
     print("Data Splitting started...")
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(document)
+    print(f"Created {len(texts)} chunks")
 
+    # using default embedding -> text-embedding-ada-002
     embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
     print("Data Ingestion started...")
     PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ["INDEX_NAME"])
