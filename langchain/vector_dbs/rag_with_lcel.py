94d0b9cdace075af05b27b5b523b20cc07f2cf64
Souparna Bose <souparnabose99@gmail.com>
2025-01-30 14:36:42 +0530

    Update project files

--- langchain/vector_dbs/rag_with_lcel.py
+++ langchain/vector_dbs/rag_with_lcel.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
-from langchain import hub
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+
 
 
 def perform_rag_steps():
@@ -13,25 +11,15 @@ def perform_rag_steps():
     embeddings = OpenAIEmbeddings()
     llm = ChatOpenAI()
 
+    # Non RAG LLM invocation
     query = "What is Vector Database in Machine Learning?"
-    #query = "What is Pinecone in Machine Learning?"
     chain = PromptTemplate.from_template(template=query) | llm
     result = chain.invoke(input={})
-
     print(result.content)
 
     vector_store = PineconeVectorStore(
         index_name=os.environ["INDEX_NAME"], embedding=embeddings
     )
-
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
-    retrieval_chain = create_retrieval_chain(
-        retriever=vector_store.as_retriever(), combine_docs_chain=combine_docs_chain
-    )
-
-    result = retrieval_chain.invoke(input={"input": query})
-    print(result)
 
     template = """Use the following pieces of context to answer the question at the end.
     If you don't know the answer, just say that you don't know, dont try to make up an answer.
@@ -45,6 +33,12 @@ def perform_rag_steps():
     Answer: """
 
     custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": vector_store.as_retriever() | format_docs, "question": RunnableThrough}
+        | custom_rag_prompt
+        | llm
+    )
 
 
 if __name__ == "__main__":
