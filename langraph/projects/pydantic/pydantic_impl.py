dfb54d36d1b1de5d54357a4b639ef191d3bd8188
Souparna Bose <souparnabose99@gmail.com>
2025-02-02 15:47:00 +0530

    Update pydantic project

--- langraph/projects/pydantic/pydantic_impl.py
+++ langraph/projects/pydantic/pydantic_impl.py
@@ -6,6 +6,9 @@ class Person(BaseModel):
     city: str
     marital_status: bool
 
+# Optional variables
+class PersonType2(BaseModel):
+
 if __name__ == "__main__":
     person = Person(name="abc", age=12, city="xyz", marital_status=False)
     print(person)
